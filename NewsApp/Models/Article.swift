9e70b4e576b8926d382c8cef42d05826e89a5543
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-23 14:39:37 -0700

    finish article model plus add the source struct within

--- NewsApp/Models/Article.swift
+++ NewsApp/Models/Article.swift
@@ -24,4 +24,52 @@ struct Article {
     var authorText: String {
         author ?? ""
     }
+    var captionText: String {
+        "\(source.name) ․ \(relativeDateFormatter.localizedString(for: publishedAt, relativeTo: Date()))"
+    }
+    
+    var descriptionText: String {
+        description ?? ""
+    }
+    
+    var imageURL: URL? {
+        guard let urlToImage = urlToImage else {
+            return nil
+        }
+        return URL(string: urlToImage)
+    }
+    
+    var articleURL: URL {
+        URL(string: url)!
+    }
+}
+
+//parsing the JSON data we get
+extension Article: Codable {}
+// comparing the data with one another so we can delete
+extension Article: Equatable {}
+// so we can pass data around
+extension Article: Identifiable {
+    var id: String {url}
+}
+
+struct Source {
+    let name: String
+}
+
+extension Source: Codable {}
+extension Source: Equatable {}
+
+extension Article {
+    static var previewData: [Article] {
+        let previewDataURL = Bundle.main.url(forResource: "news", withExtension: "json")!
+        let data = try! Data(contentsOf: previewDataURL)
+        let jsonDecoder = JSONDecoder()
+        
+        // using this format we will conform to the swift native date type
+        jsonDecoder.dateDecodingStrategy = .iso8601
+        
+        let apiResponses = try! jsonDecoder.decode(NewsAPIResponse.self, from: data)
+        return apiResponses.articles ?? []
+    }
 }
