c35ab904d653657d2ebd9685e938d44c053bd3d1
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-23 14:12:57 -0700

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Mehdi MMS on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
