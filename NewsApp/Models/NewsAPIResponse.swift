854c1989b9a9cdb8f531ea51d1bf1dd0fb5ab54b
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-23 14:46:30 -0700

    add NewsAPI response model

--- NewsApp/Models/NewsAPIResponse.swift
+++ NewsApp/Models/NewsAPIResponse.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct NewsAPIResponse: Decodable {
+    
+    let status: String
+    let totalResults: Int?
+    let articles: [Article]?
+    
+    let code: String?
+    let message: String?
+
+}
