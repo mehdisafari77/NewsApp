c0eb67fccb2f1b168b1a70fe3ba164b97b5ab51f
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-23 14:29:41 -0700

    add base code for api service

---
+++ NewsApp/Services/NewsAPI.swift
@@ -0,0 +1,27 @@
+//
+//  NewsAPI.swift
+//  NewsApp
+//
+//  Created by Mehdi MMS on 23/03/2022.
+//
+
+import Foundation
+
+struct NewsAPI {
+    
+    static let shared = NewsAPI()
+    private init() {}
+    
+    private let apiKey = "d93fc62d45a7473d9464eb2ac8f9a02a"
+    private let session = URLSession.shared
+    private let jsonDecoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        return decoder
+    }()
+    
+    func fetch(from category: Category) async throws -> [Article] {
+        try await fetchArticles(from: generralNewsURL(from: category))
+    }
+    
+}
