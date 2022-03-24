bfd523f323668738b8202f8455c6d9f1d600cba2
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-23 18:00:18 -0700

    create article list view file

---
+++ NewsApp/Views/ArticleListView.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleListView.swift
+//  NewsApp
+//
+//  Created by Mehdi MMS on 23/03/2022.
+//
+
+import SwiftUI
+
+struct ArticleListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleListView()
+    }
+}
