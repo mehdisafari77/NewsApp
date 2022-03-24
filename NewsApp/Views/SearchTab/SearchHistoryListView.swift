f5cc60ef20b5ae58c47f3c49aab5588982ed1366
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-23 18:03:44 -0700

    create search history list view

---
+++ NewsApp/Views/SearchTab/SearchHistoryListView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchHistoryListView.swift
+//  NewsApp
+//
+//  Created by Mehdi MMS on 23/03/2022.
+//
+
+import SwiftUI
+
+struct SearchHistoryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchHistoryListView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchHistoryListView()
+    }
+}
