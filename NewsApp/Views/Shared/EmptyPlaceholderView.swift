5323a509de2d586c990a2b2f0c7b3dfc7f8bca59
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-23 18:05:13 -0700

    create empty placeholder view

---
+++ NewsApp/Views/Shared/EmptyPlaceholderView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyPlaceholderView.swift
+//  NewsApp
+//
+//  Created by Mehdi MMS on 23/03/2022.
+//
+
+import SwiftUI
+
+struct EmptyPlaceholderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmptyPlaceholderView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyPlaceholderView()
+    }
+}
