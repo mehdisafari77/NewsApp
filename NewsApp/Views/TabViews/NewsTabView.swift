ca9b6cc17233416488508c4d21b0a779edc98599
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-23 17:58:43 -0700

    add news tab view

---
+++ NewsApp/Views/TabViews/NewsTabView.swift
@@ -0,0 +1,20 @@
+//
+//  NewsTabView.swift
+//  NewsApp
+//
+//  Created by Mehdi MMS on 23/03/2022.
+//
+
+import SwiftUI
+
+struct NewsTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewsTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsTabView()
+    }
+}
